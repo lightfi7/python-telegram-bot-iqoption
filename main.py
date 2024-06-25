311a1a10d251d420ce15c4dcf16e2c44f5ff80e6
Fi <158217226+lightfi7@users.noreply.github.com>
2024-06-25 08:01:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -51,5 +51,5 @@ def set_webhook():
 cache.init()
 scheduler.start()
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5002, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002, debug=True)
